4a328a2059a9f8471b789262cf09ac0ad58fa16d
NolanPereira <109583655+NolanPereira@users.noreply.github.com>
2022-09-21 00:43:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def predict():
     final_input=scaler.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output = reg_model.predict(final_input)[0]
-    return render_template("home.html",prediction_text="Your Medical Expense is {} ".format( np.around(output, decimals = 2)))
+    return render_template("home.html",prediction_text="Your Medical Expense is {} $ ".format( np.around(output, decimals = 2)))
 
 if __name__ == "__main__":
     app.run(debug=True)
